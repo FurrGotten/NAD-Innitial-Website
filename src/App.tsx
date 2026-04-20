887e9e126f575ec8fd0a42717efa31c65f2fae7f
FurrGotten <FurrGotten@gmail.com>
2026-04-20 19:08:08 +0300

    router tests 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import './App.css';
-import {BrowserRouter, HashRouter, Route, Routes} from 'react-router';
+import {HashRouter, Route, Routes} from 'react-router';
 import {Home} from './route-components';
 
 function App() {
-  const isGitHubPages = window.location.hostname.includes('github.io');
-  const Router = isGitHubPages ? BrowserRouter : HashRouter;
-
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
         {/* Add a 404 Catch-all */}
         <Route path="*" element={<div>404 - Page Not Found</div>} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
